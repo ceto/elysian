457a04e60830f81afce941056d0a1565f899d2ce
Szabó Gábor <szabogabi@gmail.com>
2019-04-16 15:41:38 +0200

    nemtom

--- lib/setup.php
+++ lib/setup.php
@@ -13,7 +13,7 @@ function setup() {
   add_theme_support('soil-clean-up');
   add_theme_support('soil-nav-walker');
   add_theme_support('soil-nice-search');
-  add_theme_support('soil-jquery-cdn');
+  //add_theme_support('soil-jquery-cdn');
   add_theme_support('soil-relative-urls');
 
   // Make theme available for translation
@@ -102,6 +102,6 @@ function assets() {
     wp_enqueue_script('comment-reply');
   }
 
-  wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['jquery'], null, true);
+  wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), [], null, true);
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
--- templates/hero.php
+++ templates/hero.php
@@ -7,6 +7,6 @@
         <a href="<?php the_field('hero_btntarget'); ?>" class="fauxbtn"><?php the_field('hero_btntext'); ?></a>
     </div>
     <figure class="hero__fig">
-        <img src="https://placehold.it/1200x1000/4e4e4e" alt="" />
+        <img src="http://placehold.it/1200x1000/4e4e4e/" alt="" />
     </figure>
 </section>
